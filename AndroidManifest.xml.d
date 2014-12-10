44c202e4793904a91293aef56052cb2a49d389b1
Benjamin <demoneaux@gmail.com>
2014-12-10 03:10:13 +0000

    Build generated on Wed Dec 10 03:09:00 UTC 2014 by Travis [ci skip]

---
+++ AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/travis/build/sheimi/SGit/SGit/bin/AndroidManifest.xml : \
+/home/travis/build/sheimi/SGit/SGit/AndroidManifest.xml \
---
+++ SGit-debug-unaligned.apk.d
@@ -0,0 +1,8 @@
+/home/travis/build/sheimi/SGit/SGit/bin/SGit-debug-unaligned.apk : \
+/home/travis/build/sheimi/SGit/SGit/bin/SGit.ap_ \
+/home/travis/build/sheimi/SGit/SGit/bin/classes.dex \
+/home/travis/build/sheimi/SGit/SGit/libs/universal-image-loader-1.9.0.jar \
+/home/travis/build/sheimi/SGit/SGit/libs/commons-io-2.4.jar \
+/home/travis/build/sheimi/SGit/SGit/libs/jsch-0.1.50.jar \
+/home/travis/build/sheimi/SGit/SGit/libs/org.eclipse.jgit-3.5.2.201411120430-r.jar \
+/home/travis/build/sheimi/SGit/SGit/libs/android-support-v13.jar \
